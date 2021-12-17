5f716c56dff7994498d6811362ce33c7f814bac1
ARYAN <agarwalaryan139@gmail.com>
2021-12-17 08:27:09 +0000

    procfile update

--- middleware/index.js
+++
@@ -1,6 +0,0 @@
-const wss = require('./socket');
-const server = require('./server');
-server.listen(8000 , () => {
-    console.log('Server started on port 8000');
-});
-
--- middleware/server.js
+++
@@ -1,4 +0,0 @@
-const app = require('./app');
-const http = require('http');
-const server = http.createServer(app);
-module.exports = server;
