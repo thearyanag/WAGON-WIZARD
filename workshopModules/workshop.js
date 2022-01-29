5be10ff0d84446a621464a835ae4e8e7836decdf
sakshitomar1410 <93925044+sakshitomar1410@users.noreply.github.com>
2022-01-29 18:28:23 +0530

    Update workshop.js

--- workshopModules/workshop.js
+++ workshopModules/workshop.js
@@ -1,7 +1,12 @@
 const workshop = require('express').Router();
 
 const tripRouter = require('./trip');
+const loginRouter = require("./login");
+const profileRouter = require("./Profile");
 
-workshop.use('/trip' , tripRouter);
+workshop.use("/trip", tripRouter);
+workshop.use("/login", loginRouter);
+workshop.use("/profile", profileRouter);
+
 
 module.exports = workshop;
